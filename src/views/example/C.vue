9431b76611313ac701a609a057da3fbbbf3adbdc
yangxiaolong <xiaolong2009yang@163.com>
2021-05-19 17:36:33 +0800

    实现5种查找组件的方法

---
+++ src/views/example/C.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    THis is a demo C
+    <D></D>
+    <E></E>
+  </div>
+</template>
+
+<script>
+import { findComponentsUp } from "../../utils/findComponent.js";
+import D from "./D.vue";
+import E from "./E.vue";
+export default {
+  name: "C",
+  components: {
+    D,
+    E,
+  },
+  data() {
+    return {
+      msg: {
+        name: "hello",
+      },
+    };
+  },
+  methods: {
+    find() {
+      let s = findComponentsUp(this, "A");
+      console.log(s);
+    },
+  },
+};
+</script>
---
+++ src/views/example/D.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    THis is a demo D
+    <button @click="find">查找E</button>
+  </div>
+</template>
+
+<script>
+import { findBrotherComponent } from "../../utils/findComponent.js";
+export default {
+  name: "D",
+  data() {
+    return {
+      msg: {
+        name: "D",
+      },
+    };
+  },
+  methods: {
+    find() {
+      let s = findBrotherComponent(this, "D");
+      console.log(s);
+    },
+  },
+};
+</script>
